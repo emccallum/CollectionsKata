cdf27db8866026c2de95f9a49405105b5bc084b7
emccallum <mccallum.ek@gmail.com>
2013-10-14 17:00:50 -0700

    creating test folder

---
+++ test/CollectionsTest.scala
@@ -0,0 +1,10 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Thoughtworks
+ * Date: 10/14/13
+ * Time: 4:59 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class CollectionsTest {
+
+}
