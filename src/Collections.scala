b10b6b3f08e2c82aaf2cfc9475b123c05ec95aab
emccallum <mccallum.ek@gmail.com>
2013-10-14 17:03:50 -0700

    Adding src file

---
+++ src/Collections.scala
@@ -0,0 +1,10 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Thoughtworks
+ * Date: 10/14/13
+ * Time: 5:03 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class Collections {
+
+}
