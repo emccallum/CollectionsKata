d3d39f0148dce13814e6b2947f539cf2d4f4a477
emccallum <mccallum.ek@gmail.com>
2013-10-14 17:31:24 -0700

    Adding tests for case 1

--- src/Collections.scala
+++ src/Collections.scala
@@ -1,3 +1,5 @@
+import java.io.PrintStream
+
 /**
  * Created with IntelliJ IDEA.
  * User: Thoughtworks
@@ -6,5 +8,12 @@
  * To change this template use File | Settings | File Templates.
  */
 class Collections {
+  val printer = new PrintStream(System.out)
 
+  def findIntersection(set1: Set[String], set2: Set[String]) {
+    if (set1.size > 0 && set2.size > 0) {
+      for (string <- set1 | set2)
+        printer.println(string)
+    }
+  }
 }
--- test/CollectionsTest.scala
+++ test/CollectionsTest.scala
@@ -1,3 +1,7 @@
+import java.io.PrintStream
+import org.specs2.mock.Mockito
+import org.specs2.mutable._
+
 /**
  * Created with IntelliJ IDEA.
  * User: Thoughtworks
@@ -5,7 +9,32 @@
  * Time: 4:59 PM
  * To change this template use File | Settings | File Templates.
  */
-class CollectionsTest {
-  println()
+class CollectionsTest extends Specification with Mockito {
+  isolated
+  val mockPrinter = mock[PrintStream]
+  val collections = new Collections{
+    override val printer = mockPrinter
+  }
+  "Two empty sets" should {
+    "print nothing" in {
+      collections.findIntersection(Set(), Set())
+      there was no(mockPrinter).println(anyString)
+    }
+  }
+
+  "An empty set and a set with content" should {
+    "print nothing" in {
+      collections.findIntersection(Set(), Set("hi"))
+      there was no(mockPrinter).println(anyString)
+    }
+  }
+
+  "Two sets with overlapping content" should {
+    "only print matching elements" in {
+      collections.findIntersection(Set("you", "hi"), Set("hi", "you", "there"))
+      there was one(mockPrinter).println("hi")
+      there was one(mockPrinter).println("you")
+    }
+  }
 
 }
